7722a44c7fb1583e39d8e3dd3db4033da6943e23
Igor Michailov <igor.m.byak@gmail.com>
2019-11-30 15:44:08 +0900

    added removing of closed connections server-side

--- server/server.js
+++ server/server.js
@@ -15,7 +15,6 @@ const ws = new WebSocket({httpServer: server});
 
 ws.on('request', req => {
   const connection = req.accept(null, req.origin);
-  // console.info(`Total connections: ${allConnections.length}`)
   //define logic after opening the connection
   console.info('NEW CONNECTION OPEN');
   //send last 20 messages
@@ -28,25 +27,32 @@ ws.on('request', req => {
   }
   //send the Server Greet
   serverGreet(connection);
-
-
-  //define closing logic
-  connection.on('close', () => {
-    console.info("CONNECTION CLOSED");
-    connection.send(JSON.stringify({"user":"Server", "server":true, "message":"Your session has ended!", "time":new Date()}));
-    // let connectionIndex = allConnections.indexOf(connection);
-    // console.log(connectionIndex);
-  });
-
-  //add connection to all list
-  allConnections.push(connection);
+  
+  //define logic of handling a message
   connection.on('message', (message) => {
     const newMessage = JSON.parse(message.utf8Data);
     newMessage.time = new Date();
     allMessages.push(newMessage);
     // console.log(`Server got a text: ${message.utf8Data}`);
-    allConnections.forEach(connect => {connect.send(JSON.stringify(newMessage))});
+    allConnections.forEach(connectionInfo => {connectionInfo.connection.send(JSON.stringify(newMessage))});
   });
+
+  //create a unique ID
+  const connectionId = allConnections.length;
+
+  //add connection to all list
+  allConnections.push({id: connectionId, connection});
+  console.info(`Total Connections: ${allConnections.length}`);
+
+  //define closing logic
+  connection.on('close', () => {
+    console.info("CONNECTION CLOSED");
+    connection.send(JSON.stringify({"user":"Server", "server":true, "message":"Your session has ended!", "time":new Date()}));
+    console.info(`removed connection ID: ${connectionId}`);
+    allConnections.splice(connectionId, 1);
+    console.info(`Total Connections: ${allConnections.length}`);
+  });
+  
 });
 
 const port = process.env.PORT || 5000;
